58a2b57c625aeeda44e708c557a3197adde8606d
이창우 <cwadven@naver.com>
2022-08-07 17:01:11 +0900

    Update 02_install_python.sh

--- jobs/02_install_python.sh
+++ jobs/02_install_python.sh
@@ -1,2 +1,2 @@
-sudo apt-get install build-essential libcairo2 libpango-1.0.0 libpangocairo-1.0.0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info libssl-dev libmysqlclient-dev
-sudo apt-get install python3-dev python3-pip python3-cffi python3-venv python3.7-dev python3.7-venv
+sudo apt-get -y install build-essential libcairo2 libpango-1.0.0 libpangocairo-1.0.0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info libssl-dev libmysqlclient-dev
+sudo apt-get -y install python3-dev python3-pip python3-cffi python3-venv python3.7-dev python3.7-venv
