f2189bffe8c4646a08798aab9aa1adf987afe1c5
이창우 <cwadven@naver.com>
2022-08-07 17:26:45 +0900

    Update 03_install_pip_modules.sh

--- jobs/03_install_pip_modules.sh
+++ jobs/03_install_pip_modules.sh
@@ -1,6 +1,6 @@
 cd $MY_PROJECT_DIRECTORY && sudo python3.7 -m venv venv
 
-source $MY_PROJECT_DIRECTORY/venv/bin/activate && pip install --upgrade pip setuptools
-source $MY_PROJECT_DIRECTORY/venv/bin/activate && pip install -r requirements.txt
-source $MY_PROJECT_DIRECTORY/venv/bin/activate && pip install wheel
-source $MY_PROJECT_DIRECTORY/venv/bin/activate && pip install uwsgi
+source $MY_PROJECT_DIRECTORY/venv/bin/activate && sudo pip install --upgrade pip setuptools
+source $MY_PROJECT_DIRECTORY/venv/bin/activate && sudo pip install -r requirements.txt
+source $MY_PROJECT_DIRECTORY/venv/bin/activate && sudo pip install wheel
+source $MY_PROJECT_DIRECTORY/venv/bin/activate && sudo pip install uwsgi
