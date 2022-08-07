5b484c1a3388bd62693a7f56e56847a7959be36a
이창우 <cwadven@naver.com>
2022-08-07 13:34:24 +0900

    Update 05_setting_nginx_link.sh

--- jobs/05_setting_nginx_link.sh
+++ jobs/05_setting_nginx_link.sh
@@ -1,9 +1,34 @@
-sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/null_blog
-sudo ln -s /etc/nginx/sites-available/null_blog /etc/nginx/sites-enabled
+sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/$PROJECT_NAME
+sudo ln -s /etc/nginx/sites-available/$PROJECT_NAME /etc/nginx/sites-enabled
 sudo rm /etc/nginx/sites-enabled/default
 
-vi /etc/nginx/nginx.conf
-# 추가 작업
+# nginx 설정 수정
+sudo sed -i 's/# server_names_hash_bucket_size 64;/server_names_hash_bucket_size 128;/g' /etc/nginx/nginx.conf
 
-sudo vi /etc/nginx/sites-available/null_blog
-# 추가 작업
+# 프로젝트 nginx uwsgi 와 연동
+
+cd /etc/nginx/sites-available/
+
+cat <<EOF | sudo tee -a $PROJECT_NAME
+upstream django {
+        server unix:$MY_PROJECT_DIRECTORY/run/uwsgi.sock;
+}
+
+server {
+        client_max_body_size 100M;
+        charset utf-8;
+
+        location / {
+                include         /etc/nginx/uwsgi_params;
+                uwsgi_pass      django;
+        }
+
+        location /static {
+                alias $MY_PROJECT_DIRECTORY/static;
+        }
+
+        location /media {
+                alias $MY_PROJECT_DIRECTORY/media;
+        }
+}
+EOF
