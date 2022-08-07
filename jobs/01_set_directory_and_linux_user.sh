39f19329231591a8084f4d619657a30c3f3b14cf
이창우 <cwadven@naver.com>
2022-08-07 13:33:08 +0900

    Update 01_set_directory_and_linux_user.sh

--- jobs/01_set_directory_and_linux_user.sh
+++ jobs/01_set_directory_and_linux_user.sh
@@ -1,6 +1,7 @@
 sudo groupadd djangogroup
 sudo useradd -g djangogroup -b /home -m -s /bin/bash django
 
-sudo chown django:djangogroup /var/www/$PROJECT_NAME
+cd /var/www && sudo git clone $GIT_URL
+sudo chown django:djangogroup $MY_PROJECT_DIRECTORY
 sudo usermod -a -G djangogroup ubuntu
-sudo chmod g+w /var/www/$PROJECT_NAME
+sudo chmod g+w $MY_PROJECT_DIRECTORY
