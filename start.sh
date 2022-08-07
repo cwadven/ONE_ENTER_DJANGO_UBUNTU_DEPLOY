829a460debda795c5db7799f28d8b3b9ab819ba6
이창우 <cwadven@naver.com>
2022-08-07 20:21:22 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -22,13 +22,28 @@ export CONFIG_SETTINGS
 export CURRENT_FOLDER
 
 # script 를 단계별로 실행
+echo "================start 00_install_nginx.sh=================="
 . "$CURRENT_FOLDER/jobs/00_install_nginx.sh"
+
+echo "================start 01_set_directory_and_linux_user.sh=================="
 . "$CURRENT_FOLDER/jobs/01_set_directory_and_linux_user.sh"
+
+echo "================start 02_install_python.sh=================="
 . "$CURRENT_FOLDER/jobs/02_install_python.sh"
+
+echo "================start 03_install_pip_modules.sh=================="
 . "$CURRENT_FOLDER/jobs/03_install_pip_modules.sh"
+
+echo "================start 04_setting_uwsgi.sh=================="
 . "$CURRENT_FOLDER/jobs/04_setting_uwsgi.sh"
+
+echo "================start 05_setting_nginx_link.sh=================="
 . "$CURRENT_FOLDER/jobs/05_setting_nginx_link.sh"
+
+echo "================start 06_setting_django_project.sh=================="
 . "$CURRENT_FOLDER/jobs/06_setting_django_project.sh"
+
+echo "================start 07_restart_nginx_uwsgi.sh=================="
 . "$CURRENT_FOLDER/jobs/07_restart_nginx_uwsgi.sh"
 
 unset GIT_URL
