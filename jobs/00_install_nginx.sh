9ca26d16262a523d2f1f24bdfd39bfcd14aa7739
이창우 <cwadven@naver.com>
2022-08-07 20:20:20 +0900

    Update 00_install_nginx.sh

--- jobs/00_install_nginx.sh
+++ jobs/00_install_nginx.sh
@@ -1,2 +1,11 @@
 sudo apt-get update
 sudo apt-get -y install nginx
+
+sudo apt-get -y install firewalld
+
+sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=443/tcp --permanent
+sudo firewall-cmd --reload
+
+sudo ufw allow 80
+sudo ufw allow 443
