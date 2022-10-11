c619afe3234f67c7e9ef68f5bf182e8d2ecb94c1
cwadven <cwadven@naver.com>
2022-10-12 02:01:26 +0900

    feat: 파일 경로에서 실행
    - 파일 경로에서 실행

--- jobs/10_migrate_django_database.sh
+++ jobs/10_migrate_django_database.sh
@@ -2,4 +2,4 @@
 . $MY_PROJECT_DIRECTORY/venv/bin/activate && pip install mysqlclient
 
 # migrate
-. $MY_PROJECT_DIRECTORY/venv/bin/activate && python manage.py migrate --noinput
+. $MY_PROJECT_DIRECTORY/venv/bin/activate && python $MY_PROJECT_DIRECTORY/manage.py migrate --noinput
