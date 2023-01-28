1e62bb0e4b9c5b742ec7503494105a954c096fc1
이창우 <cwadven@naver.com>
2023-01-28 20:00:50 +0900

    수정
    수정

--- start.sh
+++ start.sh
@@ -61,7 +61,7 @@ echo "================start 08_set_databases_with_settings.sh=================="
 . "$CURRENT_FOLDER/jobs/08_set_databases_with_settings.sh"
 
 echo "================start 09_migrate_django_database.sh.sh=================="
-. "$CURRENT_FOLDER/jobs/09_migrate_django_database.sh.sh"
+. "$CURRENT_FOLDER/jobs/09_migrate_django_database.sh"
 
 echo "================start 10_restart_nginx_uwsgi.sh=================="
 . "$CURRENT_FOLDER/jobs/10_restart_nginx_uwsgi.sh"
