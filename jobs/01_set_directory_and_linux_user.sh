45f1c58eb538851029089b44543984b414b1def0
이창우 <cwadven@naver.com>
2023-05-05 21:45:12 +0900

    Update 01_set_directory_and_linux_user.sh

--- jobs/01_set_directory_and_linux_user.sh
+++ jobs/01_set_directory_and_linux_user.sh
@@ -7,6 +7,6 @@ echo "project already exists"
 else
 cd /var/www && sudo git clone $GIT_URL
 sudo chown django:djangogroup $MY_PROJECT_DIRECTORY
-sudo usermod -a -G djangogroup ubuntu
+sudo usermod -a -G djangogroup $SERVER_USER_NAME
 sudo chmod g+w $MY_PROJECT_DIRECTORY
 fi
